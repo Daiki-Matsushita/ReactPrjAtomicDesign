cfff4c07121e13f2ccbaa994223dd2de84ab4ead
松下 大輝(Daiki Matsushita) <77758521+Daiki-Matsushita@users.noreply.github.com>
2023-10-20 21:00:40 +0900

    Initial commit

---
+++ src/components/atoms/button/SecondaryButton.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { BaseButton } from "./BaseButton";
+
+export const SecondaryButton = (props) => {
+  const { children } = props;
+  return <SButton>{children}</SButton>;
+};
+
+/**
+   ④Primaryと違うのはbackground-colorだけなので
+   それ以下はサイトとしてボタンのデザインの、デザインの統一をしたい場合
+   共通化したほうがいい
+
+   例えば、サイト全体でpaddingを大きくしたい場合、個別でデザイン持っていると
+   全ボタンファイルを一つ一つ直さないと行けなくなる
+   →★ボタン固有のデザインと共通デザインを分けることは大切
+
+
+  background-color: #11999e;
+  color: #fff;
+  padding: 6px 24px;
+  border: none;
+  outline: none;
+  border-radius: 99999px;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+
+   */
+const SButton = styled(BaseButton)`
+  background-color: #11999e;
+`;
